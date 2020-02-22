2405c153e14f50c0ca5d23282233c79cf10323c5
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-22 18:22:29 +0900

    add maker class

---
+++ ShortCodeMaker.js
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+
+
+export class ShortCodeMaker extends  React.Component {
+
+    code = "code_identifier"
+
+    render() {
+        return (
+            <></>
+        )
+    }
+}
